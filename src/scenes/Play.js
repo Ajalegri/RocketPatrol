b193658211a9bb0a1e22ace41da43a6d597c3599
Tony Alegria <ajalegri@ucsc.edu>
2021-04-12 08:21:57 -0700

    Added rocket movement and firing.

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -35,9 +35,16 @@ class Play extends Phaser.Scene {
             game.config.height - borderUISize - borderPadding, 
             'rocket'
         ).setOrigin(0.5, 0);
+
+        // define keys
+        keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+        keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
     }
 
     update() {
         this.starfield.tilePositionX -= 4;
+        this.p1Rocket.update();
     }
 }
